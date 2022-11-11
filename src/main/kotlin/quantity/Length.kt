84c3f0e5349ad6740ea8c47b3282b90ca520c59f
abhaverma <abhaverma90@gmail.com>
2022-11-11 20:48:53 +0530

    Abha | Functionality for Length subtraction.

--- src/main/kotlin/quantity/Length.kt
+++ src/main/kotlin/quantity/Length.kt
@@ -3,34 +3,29 @@ package quantity
 
 data class Length(val amount: Double, val unit: LengthUnit) {
     operator fun plus(other: Length): Length {
-        if (unit == other.unit) return copy(amount = amount + other.amount)
-        val conversionFactor = other.unit.conversionFactor(unit)
-        val convertedAmount = other.amount * conversionFactor
-        return copy(amount = amount + convertedAmount)
+        return copy(amount = amount + other.amountIn(unit))
+    }
+
+    operator fun minus(other: Length): Length {
+        return copy(amount = amount - other.amountIn(unit))
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
-
         other as Length
 
-        if (amount == other.amount && unit == other.unit) return true
-        if (unit != other.unit) {
-            val convertedAmount = unit.conversionFactor(other.unit) * amount
-            if (convertedAmount == other.amount) {
-                return true
-            } else {
-                println("expected $convertedAmount to equal ${other.amount}")
-            }
-        }
-        return false
+        return amount == other.amountIn(unit)
     }
 
     override fun hashCode(): Int {
         var result = amount.hashCode()
         result = 31 * result + unit.hashCode()
         return result
+    }
+
+    private fun amountIn(otherUnit: LengthUnit): Double {
+        return if (unit == otherUnit) amount else amount * unit.conversionFactor(otherUnit)
     }
 
 }
---
+++ src/test/kotlin/quantity/LengthSubtractionTest.kt
@@ -0,0 +1,22 @@
+package quantity
+
+import io.kotlintest.shouldBe
+import org.junit.jupiter.api.Test
+
+class LengthSubtractionTest {
+
+    @Test
+    fun `50cm minus 20cm equals 30cm`() {
+        50.0.cm - 20.0.cm shouldBe 30.0.cm
+    }
+
+    @Test
+    fun `1m minus 50cm equals 0dot5m`() {
+        1.0.m - 50.0.cm shouldBe 0.5.m
+    }
+
+    @Test
+    fun `2000cm minus 1m equals 1900cm`() {
+        2000.0.cm - 1.0.m shouldBe 1900.0.cm
+    }
+}
